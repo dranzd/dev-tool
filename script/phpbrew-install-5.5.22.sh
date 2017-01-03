a1edc8a1526c90051d91ce66c22f5e7c28e4c5be
Dranzd Viper <dranzd@gmail.com>
2017-01-03 18:14:49 +0800

    Add required libraries

--- script/phpbrew-install-5.5.22.sh
+++ script/phpbrew-install-5.5.22.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Required libraries by php5.5.22 using phpbrew installer
+# phpbrew
+# libxml-dev
+# bzip2-dev
+# libjpeg-dev
+# libpng-dev
+# libfreetype-dev
+# libmcrypt-dev
+# libreadline-dev
+# libxslt-dev
+
 CFLAGS="-I/home/user/.phpbrew/build/php-5.5.22" phpbrew install 5.5.22 +default +mysql +pdo +gd +openssl=/usr +apxs2=/usr/bin/apxs2 -- \
 --with-libdir=lib/x86_64-linux-gnu \
 --with-gd=shared \
