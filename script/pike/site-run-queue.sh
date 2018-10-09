8bd4689f12738aa2be3f216c7d92d20e204b2bba
Dranzd Viper <dranzd@gmail.com>
2018-10-09 17:27:46 +0800

    Update site-run-queue.sh

--- script/pike/site-run-queue.sh
+++ script/pike/site-run-queue.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-sudo -H -u USER -g GROUP bash -c '/usr/bin/php /path/to/app/public_html/app/public/index.php queue doctrine default --start &'
+sudo -H -u USER -g GROUP bash -c '/usr/bin/php /path/to/app/public/index.php queue doctrine default --start &'
