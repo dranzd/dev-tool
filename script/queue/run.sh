637d10a0e56dfbda3dd0dc6e1b357f8fd05da991
Dranzd Viper <dranzd@gmail.com>
2019-07-10 01:29:52 +0800

    Delete run.sh

--- script/queue/run.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-sudo -H -u www-data -g www-data bash -c '/usr/bin/php /path/to/git/app/public/index.php queue doctrine default --start &'
-echo $!
-
-# ps -eo pid,user,args --sort user | grep queue
