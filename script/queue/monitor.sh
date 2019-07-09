409222258b50dee1b2589f236996da59f11215d0
Dranzd Viper <dranzd@gmail.com>
2019-07-10 01:26:52 +0800

    Update monitor.sh

--- script/queue/monitor.sh
+++ script/queue/monitor.sh
@@ -6,7 +6,7 @@ echo -e "Found process: $COUNT"
 
 if [[ $COUNT == 0 ]]; then
     echo -e "Process not running, executing now..."
-    /path/to/script/queue/restore.sh
+    /path/to/script/queue/run.sh
 else
     echo -e "Process already running."
     echo -e "Exiting..."
