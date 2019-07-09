7e78ec8a36620633d7e973f420d0f3bb749ffad6
Dranzd Viper <dranzd@gmail.com>
2019-07-10 01:25:37 +0800

    Create monitor.sh

---
+++ script/queue/monitor.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+COUNT=`ps -fC php | grep -c "index.php queue doctrine default --start"`
+
+echo -e "Found process: $COUNT"
+
+if [[ $COUNT == 0 ]]; then
+    echo -e "Process not running, executing now..."
+    /path/to/script/queue/restore.sh
+else
+    echo -e "Process already running."
+    echo -e "Exiting..."
+fi
