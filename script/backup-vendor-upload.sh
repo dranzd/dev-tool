7974c88f548f7d2c88fc57f06025e35c1b1d40b1
Dranzd Viper <dranzd@gmail.com>
2020-04-12 14:29:41 +0800

    Update backup-vendor-upload.sh

--- script/backup-vendor-upload.sh
+++ script/backup-vendor-upload.sh
@@ -4,11 +4,12 @@ EXCLUDE_FROM_FILE=/path/to/git/source/_docs/dev/upload_vendor_copy_rsync_exclude
 LOG_FILE=/path/to/rsync.log
 USER=user
 PORT=1234
+REMOTE=remote.com
 
-SRC_URL=site.com
-SRC_DIR=/srv/www/site.com/public_html/uploads/vendor/
+SRC_URL=domain.com
+SRC_DIR="/srv/www/${SRC_URL}/public_html/uploads/vendor/"
 
-DST_DIR=/path/to/backup/latest/uploads/vendor/
+DST_DIR="/path/to/backup/${SRC_URL}/latest/uploads/vendor/"
 
 DRY_RUN='-n '
 SHOW_CMD_ONLY=false
@@ -37,14 +38,14 @@ done
 
 echo -e "Running vendor image upload backup of site \e[4m${SITE}\e[0m"
 
-CMD="rsync ${DRY_RUN} --copy-links -rtgoDCzvK --exclude-from=${EXCLUDE_FROM_FILE} -e \"ssh -l ${USER} -p ${PORT}\" ${SRC_URL}:${SRC_DIR} ${DST_DIR} > ${LOG_FILE}"
+CMD="rsync ${DRY_RUN} --copy-links -rtgoDCzvK --exclude-from=${EXCLUDE_FROM_FILE} -e \"ssh -l ${USER} -p ${PORT}\" ${REMOTE}:${SRC_DIR} ${DST_DIR}"
 
 if [[ $SHOW_CMD_ONLY == true ]]; then
-    echo -e "\e[32m${CMD} > ${LOG_FILE}\e[0m"
+    echo -e "\e[32m${CMD} >> ${LOG_FILE}\e[0m"
 else
     echo -e "** Executing command"
-    echo -e "\e[32m${CMD} > ${LOG_FILE}\e[0m"
-    eval "$CMD > $LOG_FILE"
+    echo -e "\e[32m${CMD} >> ${LOG_FILE}\e[0m"
+    eval "$CMD >> $LOG_FILE"
 fi
 
 exit 0
